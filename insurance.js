fe212f1ed1fbbf7e49fb0b7e8b817e826b54dabd
deekshaagrawal1 <deeksha231106@gmail.com>
2025-12-06 11:13:20 +0530

    Updated term insurance page with JS message

---
+++ insurance.js
@@ -0,0 +1,6 @@
+function startInsurance() {
+    const msg = document.getElementById("insurance-msg");
+    
+    msg.textContent = "We are preparing the best term insurance options for you!";
+    msg.style.display = "block";
+}
--- investor.js
+++ investor.js
@@ -1,13 +1,7 @@
-// Just to test if JS is loading
-console.log("Investor JS Loaded");
 
-// Function triggered when Start Assessment is clicked
 function startAssessment() {
-    console.log("Assessment Started!");
-
-    // Very basic DOM update
     const msgBox = document.getElementById("assessment-msg");
     
-    msgBox.textContent = "Your assessment has started! (Demo Only)";
+    msgBox.textContent = "Your assessment has started!";
     msgBox.style.display = "block";
 }
